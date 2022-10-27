6c0b2dc97d5fcdd0ab018bf35cafd55c1e1f2834
Hari Om Ahlawat <hariomahlawat@gmail.com>
2022-10-27 16:57:26 +0530

    Set the toolbar as MainActivity’s app bar

--- app/src/main/java/com/hariomahlawat/eqptcare/MainActivity.kt
+++ app/src/main/java/com/hariomahlawat/eqptcare/MainActivity.kt
@@ -2,10 +2,13 @@ package com.hariomahlawat.eqptcare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
